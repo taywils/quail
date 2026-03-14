bbf2e151f1ad13a015c42dc920a5edf2e5b01e6f
Demetrious Wilson <demetriouswilson@gmail.com>
2026-03-14 14:06:13 -0700

    Initial commit

---
+++ lib/quail.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "quail/version"
+
+module Quail
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/quail/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Quail
+  VERSION = "0.1.0"
+end

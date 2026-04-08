475460c8d7223bdcb3186b562a97a104794d53fe
Demetrious Wilson <demetriouswilson@gmail.com>
2026-04-07 20:15:05 -0700

    refactor(subscriptions): Extract hash rehydration logic into separate method
    - Move hash rehydration method definition out of build_subscription_class
    - Create dedicated define_hash_rehydration method for clarity and reusability
    - Remove unnecessary model_class variable assignment from build_subscription_class
    - Improve code organization by separating concerns between subscription building and payload transformation

--- lib/quail/resource/subscription_builder.rb
+++ lib/quail/resource/subscription_builder.rb
@@ -21,23 +21,25 @@ module Quail
       end
 
       def self.build_subscription_class(type_class, model_name, event, config)
-        model_class = model_name.constantize
         sub_class = Class.new(GraphQL::Schema::Subscription) do
           graphql_name "#{model_name}#{event.to_s.capitalize}d"
           description "Triggered when a #{model_name} is #{event}d"
           payload_type type_class
-
-          # Rehydrate Hash payloads (e.g. from delete snapshots) into model
-          # instances so computed attributes that call associations still work.
-          define_method(:update) do |**_args|
-            return object unless object.is_a?(Hash)
-
-            model_class.new(object).tap(&:readonly!)
-          end
         end
 
+        define_hash_rehydration(sub_class, model_name.constantize)
         apply_scope(sub_class, config[:scope])
         sub_class
+      end
+
+      # Rehydrate Hash payloads (e.g. from delete snapshots) into model
+      # instances so computed attributes that call associations still work.
+      def self.define_hash_rehydration(sub_class, model_class)
+        sub_class.define_method(:update) do |**_args|
+          return object unless object.is_a?(Hash)
+
+          model_class.new(object).tap(&:readonly!)
+        end
       end
 
       def self.apply_scope(sub_class, scope)
